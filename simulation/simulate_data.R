aff702361a43e1ad5efa8a48d83ea3f70ca3c2ed
Milder Hernandez <milderhc@gmail.com>
2016-06-09 18:14:28 -0500

    Updating simulate_data.R

--- simulation/simulate_data.R
+++ simulation/simulate_data.R
@@ -1,6 +1,6 @@
 #rm(list=ls())  
 
-           #####    POLI-MULTI  ######
+#####    POLI-MULTI  ######
 
 
 ########ARGUMENTOS:
@@ -23,9 +23,9 @@
 source("E:/SICS/workspace/MIRTcpp/simulation/rtnorm.r")
 
 
-simulate.poli.multi=function(size.cluster=c(25,25,25,25),
-                            dim.data=3,sample.size=1000,
-                            ncatgs=rep(3,sum(size.cluster)), seed_data = 1273  ){
+simulate.poly.multi=function(size.cluster=c(25,25,25,25),
+                             dim.data=3,sample.size=1000,
+                             ncatgs=rep(3,sum(size.cluster)), seed_data = 1273  ){
   
   #tamaño del test(num. de items)
   K=sum(size.cluster)
@@ -59,7 +59,7 @@ simulate.poli.multi=function(size.cluster=c(25,25,25,25),
   ##betas, vectores directores de cada item
   betas.1 = matrix(NA,K,dim.data)
   
-  # semilla para generar los vectores directores de cada ítem.
+  # semilla para generar los vectores directores de cada �?tem.
   seed_beta <- 100L
   set.seed(seed_beta)
   
@@ -124,10 +124,10 @@ simulate.poli.multi=function(size.cluster=c(25,25,25,25),
   
   deltas=vector("list",length = K)
   umbrales=vector("list",length = K)
- 
+  
   ##generando deltas, las distancias entre umbrales 
   for(l in 1:K){
-   deltas[[l]]=c(runif(1,-1.5,-1),rnorm((ncatgs[l]-2),0,0.4))
+    deltas[[l]]=c(runif(1,-1.5,-1),rnorm((ncatgs[l]-2),0,0.4))
   }
   
   ##generando los umbrales
@@ -142,7 +142,7 @@ simulate.poli.multi=function(size.cluster=c(25,25,25,25),
   
   ##no identificabilidad
   #for(i in 1:dim.data){
-   # Gamma[[ind1[i]]][1]=0
+  # Gamma[[ind1[i]]][1]=0
   #  }
   
   
@@ -167,17 +167,17 @@ simulate.poli.multi=function(size.cluster=c(25,25,25,25),
   # 3. test data
   #######################################################################
   
- ######################################
+  ######################################
   #  3.1 probability
   #####################################
-
+  
   ##pasteriscos:
   
   etas=list()
   thet.alpha=theta%*%t(alphas)
   for(j in 1:ncol(thet.alpha)){ 
-  etas[[j]]=thet.alpha[,j]+
-  matrix(Gamma[[j]],byrow=T,nrow=nrow(thet.alpha),ncol=length(Gamma[[j]]))
+    etas[[j]]=thet.alpha[,j]+
+      matrix(Gamma[[j]],byrow=T,nrow=nrow(thet.alpha),ncol=length(Gamma[[j]]))
   }
   
   past.1=lapply(etas,function(x){1/(1+exp(-x))})
@@ -193,14 +193,14 @@ simulate.poli.multi=function(size.cluster=c(25,25,25,25),
   set.seed(seed_data)
   Y=matrix(NA,nrow=sample.size,ncol=K)
   for(i in 1:sample.size){
-  for(j in 1:K){
-    Y[i,j]=sample(1:ncatgs[j],1,prob = probs[[j]][i,])
-     }
+    for(j in 1:K){
+      Y[i,j]=sample(1:ncatgs[j],1,prob = probs[[j]][i,])
     }
+  }
   
   params.it=list()
   for(j in 1:length(umbrales))
-  {params.it[[j]]=c(Gamma[[j]],alphas[j,])}
+  {params.it[[j]]=c(alphas[j,],Gamma[[j]])}
   
   
   retorno=list(data=Y,params.it=params.it,theta=theta,indclust1=ind1,indclust2=ind2)
@@ -208,37 +208,160 @@ simulate.poli.multi=function(size.cluster=c(25,25,25,25),
 }
 
 
+#funcion que simula los datos en poli uni
+simulate.poly.uni = function (n = 1000, nitems = 10, ncatgs = c(rep(3, 5), rep(4, 5)), model = "grm", seed_item = 1000,seed_data) 
+{
+  thetas = list()
+  umbrales = list()
+  disc=list()
+  set.seed(seed_item)
+  for ( i in 1:nitems ) {
+    beta1 = runif(n = 1, min = -2, max = -1)
+    deltas = runif(n = ncatgs[i] - 2, min = -0.4, 1)
+    umbrales[[i]] = c(cumsum(c(beta1, exp(deltas))))
+    alpha = runif(n = 1, min = 0.7, max = 2)
+    thetas[[i]] = c(umbrales[[i]] * alpha, alpha)
+    disc[[i]]=alpha
+  }
+  
+  
+  
+  z <- rnorm(n)#Vector de trazos latentes segun la distribucion
+  p <- length(thetas)#numero de items
+  nk <- sapply(thetas, length) ##numero de categorias por item
+  
+  prob <- if (model == "grm") { #aca si entra
+    thetas <- lapply(thetas, function(x) {
+      n_x <- length(x)
+      cbind(plogis(matrix(x[-n_x], n, n_x - 1, TRUE) - x[n_x] * z), 1)
+    })
+    lapply(thetas, function(x) {
+      nc <- ncol(x)
+      cbind(x[, 1], x[, 2:nc] - x[, 1:(nc - 1)])
+    })
+  }
+  
+  data <- matrix(0, n, p)
+  set.seed(seed_data)
+  for (j in 1:p) {
+    ##Extrae las muestras con las probabilidades halladas anteriormente segun el modelo
+    for (i in 1:n) data[i, j] <- sample(nk[j], 1, prob = prob[[j]][i, ])
+  }
+  
+  d = umbrales
+  for ( j in 1:p ) {
+    d[[j]] = -1 * d[[j]] * disc[[j]]
+  }
+  
+  retorno = list(data = data, params.it = mapply(function(x,y){c(x,y)},disc,d))
+  return(retorno)
+}
+
+
 
 #### FUNCTION TO SIMULATE DATA ########
 
-# It calls functions above 'simulate.poli.multi'
+# It calls functions above 'simulate.poli.multi' and 'simulate.poli.uni'
 
 simulate.data = function(size.cluster, dim.data, sample.size, ncatgs, folder, save = FALSE) {
   sims = list()
   for ( i in 1:100 ) {
-    sim=simulate.poli.multi(size.cluster=size.cluster,dim.data=dim.data, 
-                            ncatgs = ncatgs, seed_data = i)  
+    sim = list()
+    if ( dim.data > 1 )
+      sim = simulate.poly.multi(sample.size = sample.size, size.cluster = size.cluster, dim.data = dim.data,
+                                ncatgs = ncatgs, seed_data = i)  
+    else 
+      sim = simulate.poly.uni(n = sample.size, nitems = sum(size.cluster), ncatgs = ncatgs, seed_data = i)
+    
     Y = sim$data
     sims[[i]] = sim
     if ( save == TRUE )
       write.table(Y, file = paste(c(folder, i,".csv"), collapse = ""), sep = ";",col.names = FALSE, row.names = FALSE)
   }
-  sims[[1]]$params.it  
+  
+  
+  longest = 0
+  for ( i in 1:length(sims[[1]]$params.it) ) {
+    if ( length((sims[[1]]$params.it)[[i]]) > longest )
+      longest = length((sims[[1]]$params.it)[[i]])
+  }
+  
+  params = matrix(NA, length(sims[[1]]$params.it), longest)
+  
+  for ( i in 1:length(sims[[1]]$params.it) ) {
+    for ( j in 1:length((sims[[1]]$params.it)[[i]]) )
+      params[i, j] = (sims[[1]]$params.it)[[i]][[j]]
+  }
+  
+  write.table(params, file = paste(c(folder, "parameters.csv"), collapse = ""), sep = ";",col.names = FALSE, row.names = FALSE)
 }
 
 
 ##################################################################################
-#                                     2D                                         #
 ##################################################################################
-dim.data = 2
+##################################################################################
+#                                                                                #
+#                                     1D                                         #
+#                                                                                #
+##################################################################################
+##################################################################################
+##################################################################################
+
+# ESCENARIO 1
+# k = 1000
+################# 1000 x 10 ###########################
+
+simulate.data(size.cluster = c(5, 5), dim.data = 1, 
+              sample.size = 1000, ncatgs = c(rep(2, 5), rep(3, 5)),
+              folder = "IRTpp/datasets/1D/poly/escenario1/1D-poly-1000x10-", save = TRUE)
+# ESCENARIO 2
+# k = 1000
+################# 2000 x 10 ###########################
+
+simulate.data(size.cluster = c(5, 5), dim.data = 1, 
+              sample.size = 2000, ncatgs = c(rep(2, 5), rep(3, 5)),
+              folder = "IRTpp/datasets/1D/poly/escenario2/1D-poly-2000x10-", save = TRUE)
+# ESCENARIO 3
+# k = 1000
+################# 5000 x 20 ###########################
+
+simulate.data(size.cluster = c(10, 10), dim.data = 1, 
+              sample.size = 5000, ncatgs = c(rep(2, 10), rep(3, 10)),
+              folder = "IRTpp/datasets/1D/poly/escenario3/1D-poly-5000x20-", save = TRUE)
+# ESCENARIO 4
+# k = 1000
+################# 10000 x 20 ###########################
+
+simulate.data(size.cluster = c(10, 10), dim.data = 1, 
+              sample.size = 10000, ncatgs = c(rep(2, 10), rep(3, 10)),
+              folder = "IRTpp/datasets/1D/poly/escenario4/1D-poly-10000x20-", save = TRUE)
+# ESCENARIO 5
+# k = 1000
+################# 20000 x 20 ###########################
+
+simulate.data(size.cluster = c(10, 10), dim.data = 1, 
+              sample.size = 20000, ncatgs = c(rep(2, 10), rep(3, 10)),
+              folder = "IRTpp/datasets/1D/poly/escenario5/1D-poly-20000x20-", save = TRUE)
+
+
+
+##################################################################################
+##################################################################################
+##################################################################################
+#                                                                                #
+#                                     2D                                         #
+#                                                                                #
+##################################################################################
+##################################################################################
+##################################################################################
 
 # ESCENARIO 1
 # k = 1000
 ################# 1000 x 50 ###########################
 
-simulate.data(size.cluster = c(25, 25), dim.data = dim.data, 
+simulate.data(size.cluster = c(25, 25), dim.data = 2, 
               sample.size = 1000, ncatgs = c(rep(2, 25), rep(3, 25)),
-              folder = "IRTpp/datasets/2D/escenario1/2D-poly-1000x50-", save = FALSE)
+              folder = "IRTpp/datasets/2D/poly/escenario1/2D-poly-1000x50-", save = TRUE)
 
 # ESCENARIO 2
 # k = 1000
